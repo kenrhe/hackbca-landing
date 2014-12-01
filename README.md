110a7aa92a69af90031dfe982633e2f7ea0436f9
Kenneth Rhee <kenneth.h.rhee@gmail.com>
2014-12-01 10:01:14 -0500

    push

---
+++ README.md
@@ -0,0 +1,1 @@
+#HackBCA II : Coming Soon
---
+++ transition.js
@@ -0,0 +1,16 @@
+/*
+var bg = $("#background");
+var title = $("#title");
+var date = $("#date");
+var form = $("#form");
+
+TweenLite.from (bg, 1.0, { opacity: 0 });
+TweenLite.from (title, 1.3, { opacity: 0, width: "80%", delay: 1.3});
+TweenLite.from (date, 1.3, { top: "100px", opacity: 0, delay: 1.3});
+TweenLite.from (form, 1.3, { opacity: 0, width: "80%", delay: 1.3});
+*/
+
+var bg = $("#background");
+var content = $("#content");
+TweenLite.from (bg, 1.0, { opacity: 0 });
+TweenLite.from (content, 1.3, { opacity: 0, width: "80%", delay: 1.3});
